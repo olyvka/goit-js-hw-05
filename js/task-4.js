992f3e45dc23734469013997f2df433ba17bbfb1
olyvka <oliamagir06@gmail.com>
2024-01-18 14:51:47 +0200

    bug

--- js/task-4.js
+++ js/task-4.js
@@ -3,7 +3,7 @@ const getTotalBalanceByGender = (users, gender) =>
     .filter(user => user.gender === gender)
     .reduce((totalBalance, user) => totalBalance + user.balance, 0);
 
-const allUsers = [
+const allUsers1 = [
   {
     name: "Moore Hensley",
     gender: "male",
@@ -41,5 +41,5 @@ const allUsers = [
   }
 ];
 
-console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
-console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
+console.log(getTotalBalanceByGender(allUsers1, "male")); // 12053
+console.log(getTotalBalanceByGender(allUsers1, "female")); // 8863
